f4c06767a40c54111e20b67c30ebd9d74203a003
Caitlin McDonald <caitlin.adele.mcdonald@gmail.com>
2016-01-20 10:17:43 -0800

    Updating to everyone's middle names :)

--- js/areas/apt-hallway.js
+++ js/areas/apt-hallway.js
@@ -3,12 +3,12 @@ var apt_hallway = new Grid(36, 6, "apt-hallway");
 apt_hallway.addInteraction(9, 4, new Message("You don't know Colquitt that well..."), ["up"]);
 apt_hallway.addInteraction(10, 4, new Message("Colquitt's Apartment"), ["up"]);
 apt_hallway.addInteraction(15, 4, new Message("You don't know the people that live here that well..."), ["up"]);
-apt_hallway.addInteraction(16, 4, new Message("Emily & Caitlin's Apartment"), ["up"]);
+apt_hallway.addInteraction(16, 4, new Message("Margaret & Natalie's Apartment"), ["up"]);
 apt_hallway.addInteraction(22, 4, new Message("Adele & Liam's Apartment"), ["up"]);
 apt_hallway.addInteraction(27, 4, new Message("You don't know the people that live here that well..."), ["up"]);
-apt_hallway.addInteraction(28, 4, new Message("Kayla & Taylor's Apartment"), ["up"]);
-apt_hallway.addInteraction(33, 4, new Message("You don't know Cody that well..."), ["up"]);
-apt_hallway.addInteraction(34, 4, new Message("Cody's Apartment"), ["up"]);
+apt_hallway.addInteraction(28, 4, new Message("Anne & Dianne's Apartment"), ["up"]);
+apt_hallway.addInteraction(33, 4, new Message("You don't know Simon that well..."), ["up"]);
+apt_hallway.addInteraction(34, 4, new Message("Simon's Apartment"), ["up"]);
 
 apt_hallway.addExit(21, 3, ["up"], "index.html");
 apt_hallway.addExit(2, 3, ["up"], "elevator.html", "left");
