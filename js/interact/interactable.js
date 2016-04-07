a0c5f52d1f4672b353f5ae40e1414d226c15dacb
Caitlin McDonald <caitlin.adele.mcdonald@gmail.com>
2016-04-07 09:38:33 -0400

    Adds comments and function descriptions for Interactable.

--- js/interact/interactable.js
+++ js/interact/interactable.js
@@ -1,27 +1,42 @@
+/**
+  This is the base object for all objects the player can interact with.
+**/
 function Interactable() {
-  this.count = 0;
+  this.count = 0; // State management for conversation flow.
 
-  this.messages = $(".messages");
-  this.nextArrow = $(".next-arrow");
-  this.messageContent = $(".messages .content");
+  this.messages = $(".messages"); // Messages container (show/hide).
+  this.nextArrow = $(".next-arrow"); // Blinking arrow (show/hide).
+  this.messageContent = $(".messages .content"); // Messages content (adjust innerHTML).
 
-  this.options = $(".options");
-  this.selectArrow = $(".select-arrow");
-  this.optionsContent = $(".options .content");
+  this.options = $(".options"); // Options container (show/hide).
+  this.selectArrow = $(".select-arrow"); // Select arrow (show/hide).
+  this.optionsContent = $(".options .content"); // Options content (adjust innerHTML).
 
-  this.currentSelect = 0;
-  this.selectOptions = [];
+  this.currentSelect = 0; // Which option is currently selected.
+  this.originalSelect = undefined; // Will highlight an option in red if used.
+  this.selectOptions = []; // Options from which the player can select.
 }
 
+/**
+  Used to display a message on the screen.
+  @param message The message to display.
+**/
 Interactable.prototype.displayMessage = function(message) {
   this.messageContent.html(message);
 }
 
+/**
+  Generates and displays the HTML for any conversation options.
+  @param options The list of options to display.
+  @param mark    Boolean, whether or not mark the original option in red.
+**/
 Interactable.prototype.displayOptions = function(options, mark) {
-  mark = mark || false;
   this.selectOptions = options;
+
+  // Builds the list's HTML.
   var optionsHtml = "";
   for (var i = 0; i < options.length; i ++) {
+    mark = mark || false;
     if (mark && i == this.originalSelect) {
       optionsHtml += "<div class='current-selection'>";
     } else {
@@ -33,6 +48,9 @@ Interactable.prototype.displayOptions = function(options, mark) {
   this.optionsContent.html(optionsHtml);
 }
 
+/**
+  Moves the player's selection arrow and updates Interactable.currentSelect.
+**/
 Interactable.prototype.arrowUp = function() {
   if (this.currentSelect > 0) {
     this.currentSelect -= 1;
@@ -40,6 +58,9 @@ Interactable.prototype.arrowUp = function() {
   }
 }
 
+/**
+  Moves the player's selection arrow and updates Interactable.currentSelect.
+**/
 Interactable.prototype.arrowDown = function() {
   if (this.currentSelect < this.selectOptions.length - 1) {
     this.currentSelect += 1;
@@ -47,6 +68,9 @@ Interactable.prototype.arrowDown = function() {
   }
 }
 
+/**
+  Sets the location of the player's selection arrow.
+**/
 Interactable.prototype.setSelectArrow = function() {
   this.selectArrow.css("top", (4 + 10 * this.currentSelect) * MULT);
 }
