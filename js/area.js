ff8c30bc793239cf4a6b88e413d976829cf1a2d0
Caitlin McDonald <caitlin.adele.mcdonald@gmail.com>
2016-04-07 13:53:42 -0400

    Comments + function descriptions!

--- js/area.js
+++ js/area.js
@@ -1,17 +1,30 @@
+/**  
+  The Area object manages the grid and spaces of a room/area.
+  @param width  The width of the area (in blocks).
+  @param height The height of the area (in blocks);
+  @param name   The name of the area. Should also be the name of the svg.
+  @param mask   Whether or not a mask file should be processed.
+**/
 function Area(width, height, name, mask) {
-  this.width = width;
-  this.height = height;
-  this.name = name;
+  this.width = width;   // Width of the area in blocks.
+  this.height = height; // Height of the area in blocks.
+  this.name = name;     // Name of the area.
 
-  this.area = $(".area");
+  this.areaEl = $(".area"); // The area element to manipulate.
 
+  // Following values are set in Area.setPlacementLimits
+  this.maxLeft = undefined;   // Greatest left position (in blocks) area should go.
+  this.minLeft = undefined;   // Smallest left position (in blocks) area should go.
+  this.maxBottom = undefined; // Greatest bottom position (in blocks) area should go.
+  this.minBottom = undefined; // Smallest bottom position (in blocks) area should go.
   this.setPlacementLimits();
-  window.sessionStorage.setItem("room", name);
-  this.items = [];
-  this.areaElements = [];
-  this.positionData = {};
 
-  this.grid = new Array(width);
+  this.items = [];        // Collection of show zone items.
+  this.elements = [];     // HTML elements added to area.
+  this.positionData = {}; // Player positioning based on entrances from other areas.
+
+  this.grid = new Array(width); // Collection of spaces in the area.
+  // Create a new Space object for every grid coordinate.
   for (var i = 0; i < width; i++) {
     this.grid[i] = new Array(height);
     for (var n = 0; n < height; n++) {
@@ -19,87 +32,142 @@ function Area(width, height, name, mask) {
     }
   }
 
-  var image = new Image();
-  image.crossOrigin = "Anonymous";
-  var grid = this;
+  // Only do mask processing if necessary.
+  if (mask) {
+    // We'll load the mask as an image and then check the image data.
+    var image = new Image();
+    image.crossOrigin = "Anonymous";
 
-  image.onload = function() {
-    var canvas = document.createElement('canvas');
-    canvas.width = image.width;
-    canvas.height = image.height;
+    // Process image data when mask image loads.
+    var area = this;
+    image.onload = function() {
+      var canvas = document.createElement('canvas');
+      canvas.width = image.width;
+      canvas.height = image.height;
 
-    var context = canvas.getContext('2d');
-    context.drawImage(image, 0, 0);
+      var context = canvas.getContext('2d');
+      context.drawImage(image, 0, 0);
 
-    var imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-    
-    for (var blockX = 0; blockX < grid.width; blockX++) {
-      for (var blockY = 0; blockY < grid.height; blockY++) {
-        var xLeft = blockX * BLOCK;
-        var yTop = blockY * BLOCK;
+      var imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+      
+      // NOTE: Image data is processed with 0, 0 being the top left coordinate.
+      //       Our grid is stores with 0, 0 being the bottom left.
+      //       Y-coord is manipulated at the end, when saving the data
+      // Iterating through BLOCKS, not individual pixels.
+      for (var blockX = 0; blockX < area.width; blockX++) {
+        for (var blockY = 0; blockY < area.height; blockY++) {
+          // Find the (inclusive) bounds of the block.
+          var xLeft = blockX * BLOCK;
+          var yTop = blockY * BLOCK;
+          var xRight = xLeft + BLOCK - 1;
+          var yBottom = yTop + BLOCK - 1;
+ 
+          // Find middle x and y coordinates.
+          // Don't want to use pixel at corner of block.
+          var xMid = xLeft + 8;
+          var yMid = yTop + 8;
 
-        var xMid = xLeft + 8;
-        var xRight = xLeft + BLOCK - 1;
-        var yMid = yTop + 8;
-        var yBottom = yTop + BLOCK - 1;
+          // Using combinations of coordinates, find index in image data.
+          var lfCheck = (yMid*imageData.width + xLeft) * 4;
+          var upCheck = (yTop*imageData.width + xMid) * 4;
+          var rtCheck = (yMid*imageData.width + xRight) * 4;
+          var dwCheck = (yBottom*imageData.width + xMid) * 4;
 
-        var lfCheck = (yMid*imageData.width + xLeft) * 4;
-        var upCheck = (yTop*imageData.width + xMid) * 4;
-        var rtCheck = (yMid*imageData.width + xRight) * 4;
-        var dwCheck = (yBottom*imageData.width + xMid) * 4;
+          // Check if R value of pixel at each location is NOT "ff"
+          // If the R value is anything else, should be non-white and indicate a mask.
+          var lfBlocked = imageData.data[lfCheck].toString(16) != "ff";
+          var upBlocked = imageData.data[upCheck].toString(16) != "ff";
+          var rtBlocked = imageData.data[rtCheck].toString(16) != "ff";
+          var dwBlocked = imageData.data[dwCheck].toString(16) != "ff";
 
-        var lfBlocked = imageData.data[lfCheck].toString(16) != "ff";
-        var upBlocked = imageData.data[upCheck].toString(16) != "ff";
-        var rtBlocked = imageData.data[rtCheck].toString(16) != "ff";
-        var dwBlocked = imageData.data[dwCheck].toString(16) != "ff";
+          // Collect blocked directions.
+          var blockedDirections = [];
+          if (lfBlocked) { blockedDirections.push("rt"); }
+          if (upBlocked) { blockedDirections.push("dw"); }
+          if (rtBlocked) { blockedDirections.push("lf"); }
+          if (dwBlocked) { blockedDirections.push("up"); }
 
-        var blockedDirections = [];
-        if (lfBlocked) { blockedDirections.push("rt"); }
-        if (upBlocked) { blockedDirections.push("dw"); }
-        if (rtBlocked) { blockedDirections.push("lf"); }
-        if (dwBlocked) { blockedDirections.push("up"); }
-
-        if (blockedDirections.length > 0) {
-          grid.space(blockX, grid.height - blockY - 1).setBlocked(blockedDirections);
+          // If there are indeed blocked directions, set them on the related Space.
+          if (blockedDirections.length > 0) {
+            area.space(blockX, area.height - blockY - 1).setBlocked(blockedDirections);
+          }
         }
       }
-    }
-  };
-  if (mask) { image.src = "img/areas/" + this.name + "_mask.png"; }
+    };
+    
+    // Onload will be called after image is set.
+    image.src = "img/areas/" + this.name + "_mask.png";
+  }
 }
 
-Area.prototype.build = function(removeDivs) {
+/**
+  Called when we've entered an area and the HTML should be built out.
+  @param removeEls The elements set by the previous area that should be removed.
+**/
+Area.prototype.build = function(removeEls) {
+  // Save that we've entered a new room.
+  window.sessionStorage.setItem("room", name);
+  // Set the background image to the area's svg.
   $(".area-img").attr("src", "img/areas/" + this.name + ".svg");
 
-  removeDivs = removeDivs || [];
-  for (var i = 0; i < removeDivs.length; i++) {
-    $(removeDivs[i]).remove();
+  // Remove previous area's elements.
+  removeEls = removeEls || [];
+  for (var i = 0; i < removeEls.length; i++) {
+    $(removeEls[i]).remove();
   }
 
-  var leftOffset = 11 - this.width > 0 ? (11 - this.width) / 2 : 0;
-  var bottomOffset = 11 - this.height > 0 ? (11 - this.height) / 2 : 0;
+  // Set the height and width of the area element.
+  this.areaEl.css("width", (this.width * BLOCK * MULT).toString() + "px")
+             .css("height", (this.height * BLOCK * MULT).toString() + "px");
 
-  this.area.css("width", (this.width * BLOCK * MULT).toString() + "px")
-           .css("height", (this.height * BLOCK * MULT).toString() + "px")
-           .css("left", (leftOffset * BLOCK * MULT).toString() + "px")
-           .css("bottom", (bottomOffset * BLOCK * MULT).toString() + "px");
-
-  for (var i = 0; i < this.areaElements.length; i++) {
-    this.area.append(this.areaElements[i]);
+  // Add all the elements created upon intialization of the area.
+  for (var i = 0; i < this.elements.length; i++) {
+    this.areaEl.append(this.elements[i]);
   }
 }
 
+/**
+  Called to set the max and min values to position the area within the game window.
+**/
 Area.prototype.setPlacementLimits = function() {
-  this.maxLeft = this.width < 11 ? (11 - this.width) / 2 : 0;
-  this.minLeft = 
-    this.width < 11 ? (11 - this.width) / 2 : -1 * (this.width - 11);
+  var GAME_WIDTH = 11;
+  var GAME_HEIGHT = 11;
 
-  this.maxBottom = this.height < 11 ? (11 - this.height) / 2 : 0;
-  this.minBottom = 
-    this.height < 11 ? (11 - this.height) / 2 : -1 * (this.height - 11);
+  // If the area isn't as wide or as tall as the game window, the max and min
+  // should be the same (and centered within the game window). If the area is
+  // wider or taller than the game window, the greatest offset should be 0 and 
+  // the smallest offset should be all of the area hidden but the width or height
+  // of the game window.
+
+  if (this.width < GAME_WIDTH) {
+    this.maxLeft = (GAME_WIDTH - this.width) / 2;
+    this.minLeft = this.maxLeft;
+  } else {
+    this.maxLeft = 0;
+    this.minLeft = -1 * (this.width - GAME_WIDTH);
+  }
+
+  if (this.height < GAME_HEIGHT) {
+    this.maxBottom = (GAME_HEIGHT - this.height) / 2;
+    this.minBottom = this.maxLeft;
+  } else {
+    this.maxBottom = 0;
+    this.minBottom = -1 * (this.height - GAME_HEIGHT);
+  }
 }
 
+/**
+  Adds information about player positioning when entering an area.
+  @param areaFrom The area the player is coming from.
+  @param door     The door the player is coming from (optional, can be null).
+  @param x        The x coordinate to be placed at.
+  @param y        The y coordinate to be placed at.
+  @param face     The direction to be facing.
+**/
 Area.prototype.addPositionData = function(areaFrom, door, x, y, face) {
+  // Construct a key for the postionData map
+  // Concatinates the are and door (if there is a door).
+  // ex. apt-1-left, apt-1-right, studio
   var key = areaFrom;
   if (door != null && door != "") {
     key += "-" + door;
@@ -107,39 +175,77 @@ Area.prototype.addPositionData = function(areaFrom, door, x, y, face) {
   this.positionData[key] = { x: x, y: y, face: face};
 }
 
+/**
+  Find where the player should be placed give they came from a certain area/door.
+  @param areaFrom The area the player is coming from.
+  @param door     The door the player is coming from.
+**/
 Area.prototype.getPositionData = function(areaFrom, door) {
+  // Construct key from information.
   var key = areaFrom;
   if (door != null && door != "") {
     key += "-" + door;
   }
 
+  // If we have that particular key.
   if (key in this.positionData) {
     return this.positionData[key];
+
+  // Don't have the key, but have a default location.
   } else if (this.positionData.default != undefined) {
     return this.positionData.default;
+
+  // Don't have the key or a default
+  // Shouldn't arrive at this unless something was incorrectly set up.
   } else {
     return { x: 0, y: 0, face: "up"};
   }
 }
 
+/**
+  Returns a space from the Area's grid.
+  @param x The x block coordinate.
+  @param y The y block coordinate.
+  @return Space object or undefined if not a valid x, y coordinate pair.
+**/
 Area.prototype.space = function(x, y) {
   return this.validZone(x,y) ? this.grid[x][y] : undefined;
 }
 
+/** 
+  Checks if the x, y coordinate is within the Area's grid.
+  @param x The x block coordinate.
+  @param y The y block coordinate.
+  @return Boolean
+**/
 Area.prototype.validZone = function(x, y) {
-  return x >= 0 
-    && x <= this.width -1 
-    && y >= 0 
-    && y <= this.height - 1;
+  return x >= 0 &&
+         x <= this.width - 1 &&
+         y >= 0 &&
+         y <= this.height - 1;
 }
 
-Area.prototype.addShowZone = function(height, width, item, startCoord, showCoords) {
+/**
+  Adds new show zones to an area.
+  @param width      Width of the item to show.
+  @param height     Height of the item to show.
+  @param item       The name of the item.
+  @param startCoord Starting coordinate to place item at.
+  @param showCoords Coordinates at which to show the item.
+**/
+Area.prototype.addShowZone = function(width, height, item, startCoord, showCoords) {
+  // Add to item collection.
   this.items.push("item-" + item);
 
+  /* Sample HTML
+     <div class="item item-bed">
+       <img src="img/items/studio/bed.svg">
+     </div>
+  */
   var div = document.createElement("div");
   $(div).addClass("item item-" + item)
-        .css("height", (height * BLOCK * MULT).toString() + "px")
         .css("width", (width * BLOCK * MULT).toString() + "px")
+        .css("height", (height * BLOCK * MULT).toString() + "px")
         .css("left", (startCoord[0] * BLOCK * MULT).toString() + "px")
         .css("bottom", (startCoord[1] * BLOCK * MULT).toString() + "px")
 
@@ -148,13 +254,22 @@ Area.prototype.addShowZone = function(height, width, item, startCoord, showCoord
   $(img).attr("src", src);
   $(div).append(img);
 
-  this.areaElements.push(div);
+  // Add to element collection.
+  this.elements.push(div);
 
+  // Set necessary spaces as show zones.
   for (var i = 0; i < showCoords.length; i++) {
     this.space(showCoords[i][0], showCoords[i][1]).setShowZone("item-" + item);
   }
 }
 
+/**
+  Adds an interaction to a Space.
+  @param x           The x coordinate to add interaction at.
+  @param y           The y coordinate to add interaction at.
+  @param interaction Interactable object.
+  @param dir         Directions from which interaction are valid (optional).
+**/
 Area.prototype.addInteraction = function(x, y, interaction, dir) {
   dir = dir || [];
   this.space(x, y).setInteractionZone(interaction)
@@ -163,9 +278,22 @@ Area.prototype.addInteraction = function(x, y, interaction, dir) {
   }
 }
 
+/**
+  Creates the NPC element, then calles Space.addInteraction.
+  @param x   The x coordinate to add interaction at.
+  @param y   The y coordinate to add interaction at.
+  @param npc NPC object.
+  @param dir Directions from which interaction are valid (optional).
+**/
 Area.prototype.addNPC = function(x, y, npc, dir) {
-  dir = dir || [];
-  
+  /* Sample HTML
+     <div class="npc npc-liam">
+       <div class="avatar"></div>
+       <div class="shadow">
+         <img src="img/characters/shadow_lg.svg">
+       </div>
+     </div>
+  */
   var div = document.createElement("div");
   $(div).addClass("npc npc-" + npc.name)
         .css("left", ((x * BLOCK - 3) * MULT).toString() + "px")
@@ -176,6 +304,7 @@ Area.prototype.addNPC = function(x, y, npc, dir) {
   npc.avatar = new Avatar($(avatar));
   $(div).append(avatar);
 
+  // Some NPCs don't have shadows.
   if (npc.shadow != undefined) {
     var shadow = document.createElement("div");
     $(shadow).addClass("shadow");
@@ -186,24 +315,42 @@ Area.prototype.addNPC = function(x, y, npc, dir) {
     $(div).append(shadow);
   }
 
-  this.areaElements.push(div);
+  // Add to element collection.
+  this.elements.push(div);
 
+  // Call addInteraction with parameters.
   this.addInteraction(x, y, npc, dir);
 }
 
+/**
+  Adds new event zones to an area.
+  @param startCoord Array of x, y coordinate (bottom left).
+  @param endCoord   Array of x, y coordinate (top right).
+  @param event      The event for the Spaces.
+**/
 Area.prototype.addEventZone = function(startCoord, endCoord, event) {
+  // Go from bottom left to top right coordinates (inclusive).
   for (var x = startCoord[0]; x < endCoord[0] + 1; x++) {
     for (var y = startCoord[1]; y < endCoord[1] + 1; y++) {
       this.space(x, y).setEvent(event);
     }
   }
-
-  eventAreaElements = event.getAreaElements();
-  for (var i = 0; i < eventAreaElements.length; i++) {
-    this.areaElements.push(eventAreaElements[i]);
+  
+  // Add any elements the event needs created.
+  eventElements = event.getElements();
+  for (var i = 0; i < eventElements.length; i++) {
+    this.elements.push(eventElements[i]);
   }
 }
 
+/**
+  Adds an exit to an area.
+  @param x        The x coordinate of the exit-adjacent Space.
+  @param y        The y coordinate of the exit-adjacent Space.
+  @param dir      The direction of the exit from the Space.
+  @param location The area to exit to.
+  @param door     The door exiting from (optional).
+**/
 Area.prototype.addExit = function(x, y, dir, location, door) {
   door = door || ""
   this.space(x, y).setExit(dir, location);
--- js/game.js
+++ js/game.js
@@ -1,20 +1,38 @@
+/**
+  The master Game object. Manages all other objects and moving between areas.
+**/
 function Game() {
-  this.game = $("#game");
-  this.player = $("#player");
-  this.avatar = new Avatar($("#avatar"));
-  this.areaEl = $(".area");
-  this.areas = {}
-  this.keyboardController();
+  this.gameEl = $("#game"); // Game element.
+  this.player = $("#player"); // Player element.
+  this.avatar = new Avatar($("#avatar")); // Avatar element.
+  this.areaEl = $(".area"); // Area element.
+
+  this.areas = {}         // Map of area names to their Area objects.
+  this.focus = undefined; // The current focus.
+  this.event = undefined; // The current event.
+
+  //The following are initialized in Game.start
+  this.messager = undefined; // The Game's messaging system.
+  this.x = undefined;        // The player's current x location.
+  this.y = undefined;        // The player's current y location.
+  this.face = undefined;     // The player's current faced direction.
+
+  // Start the keyboard controller for key events (see js/keys.js).
+  this.keyboardController(); 
 }
 
+/**
+  Called to start the game!
+  @param startX    The x coordinate to start the player at.
+  @param startY    The y coordinate to start the player at.
+  @param startFace The direction to start the player facing.
+  @param area      The area to start in.
+**/
 Game.prototype.start = function(startX, startY, startFace, area) {
   this.moveToArea(area);
-  this.messager = new Message("");
+  this.messager = new Message(""); 
 
-  this.focus = undefined;
-  this.event = undefined;
-
-  this.x = startX;
+  this.x = startX; 
   this.y = startY;
   this.face = startFace;
 
@@ -22,28 +40,43 @@ Game.prototype.start = function(startX, startY, startFace, area) {
   this.moveToSpace(this.x, this.y, this.face);
 }
 
+/**
+  Adds an area to the Game.areas map.
+  @param key The key (name) of the Area object.
+  @param area Area object.
+**/
 Game.prototype.addArea = function (key, area) {
   this.areas[key] = area;
 }
 
+/**
+  Set up for transitioning to a new area in the game.
+  @param area The name of the area to move to.
+**/
 Game.prototype.moveToArea = function(area) {
+  // Initialize values to empty.
   var from = "";
   var door = "";
 
+  // New area to move to.
   var newArea = this.areas[area];
+  // If it's not a new game and we're in an area.
   if (this.area != undefined) {
     from = this.area.name;
+    // Save the current area we're traveling from.
     window.sessionStorage.setItem("from", this.area.name);
     door = window.sessionStorage.getItem("door");
-    newArea.build(this.area.areaElements);
+    newArea.build(this.area.elements);
   } else {
     newArea.build();
   }
   this.area = newArea;
   window.sessionStorage.setItem("area", area);
-
-  var positionData = this.area.getPositionData(from, door);
+  // Remove door data until it's set again by a specific exit door.
   window.sessionStorage.removeItem("door");
+
+  // Find position data to enter area.
+  var positionData = this.area.getPositionData(from, door);
   this.x = positionData.x;
   this.y = positionData.y;
   this.face = positionData.face;
@@ -51,17 +84,25 @@ Game.prototype.moveToArea = function(area) {
   this.faceDir(this.face);
   this.moveToSpace(this.x, this.y, this.face);
 
-  this.status = "loading";
-  this.game.removeClass("visible");
+  // Fade in/out animation between areas.
+  this.gameEl.removeClass("visible");
   var that = this;
   window.setTimeout(function() {
-    that.game.addClass("visible");
+    that.gameEl.addClass("visible");
   }, 300);
+  // Lock game mode until new area is totally loaded.
+  this.status = "loading";
   window.setTimeout(function() {
     that.status = "free";
   }, 800);
 }
 
+/**
+  Called to change direction player is facing.
+  Mostly kept around for neatness of firing the direction change event.
+  (Since the keyboardController has access to the avatar directly)
+  @param dir The direction to face.
+**/
 Game.prototype.faceDir = function(dir) {
   this.face = dir;
   window.sessionStorage.setItem("face", this.face);
@@ -91,28 +132,48 @@ Game.prototype.faceDir = function(dir) {
   }
 }
 
+/**
+  Moves player one space to the left (moveToSpace determines validity).
+**/
 Game.prototype.moveLeft = function() {
   this.faceDir("lf");
   this.moveToSpace(this.x - 1, this.y, "lf");
 }
 
+/**
+  Moves player one space up (moveToSpace determines validity).
+**/
 Game.prototype.moveUp = function() {
   this.faceDir("up");
   this.moveToSpace(this.x, this.y + 1, "up");
 }
 
+/**
+  Moves player one space to the right (moveToSpace determines validity).
+**/
 Game.prototype.moveRight = function() {
   this.faceDir("rt");
   this.moveToSpace(this.x + 1, this.y, "rt");
 }
 
+/**
+  Moves player one space down (moveToSpace determines validity).
+**/
 Game.prototype.moveDown = function() {
   this.faceDir("dw");
   this.moveToSpace(this.x, this.y - 1, "dw");
 }
 
+/**
+  Moves player to a new space if it's valid. Activates show zones, event zones, etc.
+  @param toX     The proposed x coordinate.
+  @param toY     The proposed y coordinate.
+  @param fromDir Direction traveling from.
+**/
 Game.prototype.moveToSpace = function(toX, toY, fromDir) {
+  // First check if we're trying to move into an exit.
   if (this.area.space(this.x, this.y).hasExitAdjacent(this.face)) {
+    // If there is a specific door we're exiting, save it.
     if (space.hasExitDoor()) {
       window.sessionStorage.setItem("door", space.door())
     }
@@ -120,32 +181,41 @@ Game.prototype.moveToSpace = function(toX, toY, fromDir) {
     return;
   }
 
+  // Check if we're trying to enter a valid zone.
   if (this.validZone(toX, toY)) {
-    space = this.area.grid[toX][toY];
+    space = this.area.space(toX, toY);
 
+    // Make sure we can travel into that space from our current direction.
     if (!space.isBlocked(fromDir)) {
       this.x = toX;
       this.y = toY;
+      // Keeping around for debugging purposes.
       console.log(this.x, this.y);
+      // Save our current location (for page reload).
+      // Will likely be removed when saving game is possible (no need to save every step).
       window.sessionStorage.setItem("x", this.x);
       window.sessionStorage.setItem("y", this.y);
 
+      // If we're entering an event space, trigger it!
       if (space.hasEvent() && this.event == undefined) {
         this.event = space.event();
         this.event.begin(this.x, this.y, this.face);
+
+      // If we're still in an event space, fire movement event.
       } else if (space.hasEvent()) {
         this.event.fireMove(this.x, this.y);
+
+      // If we're not in an event space, but have an event saved, end it!
       } else if (this.event != undefined) {
         this.event.end();
         this.event = undefined;
       }
 
-      window.sessionStorage.setItem("x", this.x);
-      window.sessionStorage.setItem("y", this.y);
-
+      // Set the player's new position.
       this.player.css("left", (this.x * BLOCK - 3) * MULT);
       this.player.css("bottom", (this.y * BLOCK - 1) * MULT);
       
+      // This will be moved to Area soon!
       if (this.x <= 4) {
         this.areaEl.css("left", this.area.maxLeft * BLOCK * MULT);
       } else if (this.x >= this.area.width - 5) {
@@ -167,17 +237,28 @@ Game.prototype.moveToSpace = function(toX, toY, fromDir) {
   } 
 }
 
+/** 
+  Checks if space is a valid zone of the game (accounts for walls).
+  @param x The x coordinate.
+  @param y The y coordinate.
+**/
 Game.prototype.validZone = function(x, y) {
-  return x >= 0 
-    && x <= this.area.width - 1 
-    && y >= 0 
-    && y <= this.area.height - 3;
+  return x >= 0
+      && x <= this.area.width - 1
+      && y >= 0
+      && y <= this.area.height - 3;
 }
 
+/**
+  Handles all things related to show zones.
+**/
 Game.prototype.showZone = function() {
+  // Hide all items except the current item (if it exists).
+  // To be honest I can't 100% remember my logic here.
+  // Should likely be refactored. Layers are hard.
   var that = this;
   window.setTimeout(function() {
-    item = that.area.grid[that.x][that.y].itemToShow();
+    item = that.area.space(that.x, that.y).itemToShow();
     for (i = 0; i < that.area.items.length; i++) {
       if (item != that.area.items[i]) {
         $("." + that.area.items[i]).hide();
@@ -185,38 +266,49 @@ Game.prototype.showZone = function() {
     }
   }, 350);
 
-  currentSpace = this.area.grid[this.x][this.y];
+  currentSpace = this.area.space(this.x, this.y);
   if (currentSpace.isShowZone()) {
     $("." + currentSpace.itemToShow()).show();
   }
 }
 
+/**
+  Handles a player trying to interact with something
+**/
 Game.prototype.interact = function() {
   switch(game.status) {
+    // If the game is in free mode, try and interact with something.
     case "free":
       currentSpace = this.area.space(this.x, this.y);
 
+      // Find the x, y coordinate we're facing.
       faceX = this.face == "lf" ? this.x - 1 : this.x;
       faceX = this.face == "rt" ? this.x + 1 : faceX;
       faceY = this.face == "up" ? this.y + 1 : this.y;
       faceY = this.face == "dw" ? this.y - 1 : faceY;
-
       faceSpace = this.area.space(faceX, faceY);
+
+      // If we're in an interact zone, focus on that.
       if (currentSpace.isInteractZone()) {
         this.focus = currentSpace.interaction();
         this.status = currentSpace.interaction().interact(this.face) || "free";
+
+      // If we're facing an interactable space.
       } else if (faceSpace != undefined && faceSpace.canInteract(this.face)) {
         this.focus = faceSpace.interaction();
         this.status = faceSpace.interaction().interact(this.face) || "free";
       }
       break;
 
+    // If game is in conversation mode, advance the conversation.
     case "convo":
       this.status = this.focus.interact(this.face) || "free";
+
       if (this.status == "free") {
         this.focus = undefined;
       } else if (this.status == "exit") {
         this.exit(this.focus.exitTo());
+        this.focus = undefined;
       }
       break;
 
@@ -225,7 +317,12 @@ Game.prototype.interact = function() {
   }
 }
 
+/**
+  Exits to another area.
+  @param exitTo The name of the area to go to.
+**/
 Game.prototype.exit = function(exitTo) {
+  // Collection of areas character can't enter.
   var cantGo = [
     "colquitt-natalie", 
     "margaret-kayla", 
@@ -234,6 +331,7 @@ Game.prototype.exit = function(exitTo) {
     "elevator-roof"
   ];
 
+  // If our exit goes somewhere we can't, customize the message.
   if ($.inArray(exitTo, cantGo) != -1) {
     if (exitTo == "colquitt-natalie" 
       || exitTo == "margaret-kayla" 
