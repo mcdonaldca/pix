326264b5983e2b3166e933c9310c2e1347371803
Caitlin McDonald <caitlin.adele.mcdonald@gmail.com>
2016-04-19 16:14:11 -0400

    Accidentally left in a log.

--- js/time.js
+++ js/time.js
@@ -65,7 +65,6 @@ Time.prototype.startTime = function() {
 Time.prototype.tick = function(time) {
   return function() {
     // Only increase time if the timer should be active.
-    console.log(game.status);
     if (time.active && game.status == "free") {
       time.time += 100;
       // Increment time every ten seconds.
