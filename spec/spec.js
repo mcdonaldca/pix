363c46993fe19bd6fddfd9ce73f3bd427baa06dd
Caitlin McDonald <caitlin.adele.mcdonald@gmail.com>
2017-02-12 18:17:46 -0500

    Adds some (admittedly rough) comments to the Spec class.

--- spec/spec.js
+++ spec/spec.js
@@ -1,18 +1,35 @@
+/**
+  Method to create a test suite for a class.
+  @param className The class of the html element to display test results.
+**/
 function Spec(className) {
   this.testEl = $(className);
   this.results = [];
   this.success = true;
 
-  var specObj = this;
+  var spec = this;
   this.testEl.click(function() {
-    $('.results').html(specObj.resultsHTML());
+    $('.results').html(spec.resultsHTML());
   });
 }
 
+
+
+/**
+  Constructs test suite results output to be displayed to the user.
+  @return The test results HTML.
+**/
 Spec.prototype.resultsHTML = function() {
   return '<h2>' + this.testEl.text() + '</h2>' + this.output();
 };
 
+
+
+/**
+  Returns the error message for a failing test.
+  @param error Object containing information about the error.
+  @return The error message HTML.
+**/
 Spec.prototype.errorMessageHTML = function(error = {}) {
   var errorMessage = '<div class="message">' + error.message + '</div>';
   if (error.expected || error.actual) {
@@ -25,6 +42,14 @@ Spec.prototype.errorMessageHTML = function(error = {}) {
   return errorMessage;
 };
 
+
+
+/**
+  The results for a specific test in the test block.
+  @param testName    The name of the test
+  @param testResults The results (HTML) of the test.
+  @return The HTML of the test's results, whether passed or failed.
+**/
 Spec.prototype.testResultsHTML = function(testName, testResults) {
   var testIndicator = '<div class="test-output__indicator"></div>'
   var testName = '<div class="test-output__name">' + testName + '</div>';
@@ -36,6 +61,12 @@ Spec.prototype.testResultsHTML = function(testName, testResults) {
   return '<div class="results__test-output ' + passedClass + '">' + testOutput + '</div>'
 };
 
+
+/**
+  The title HTML for a specfic test block within the entire suite.
+  @param description The description of the test block.
+  @return The HTML for a test block.
+**/
 Spec.prototype.testDescribeHTML = function(description) {
   var describeBlockOuter = '<div class="results__describe-block">'
   var testOutputOuter = '<div class="results__test-output description">'
@@ -43,11 +74,23 @@ Spec.prototype.testDescribeHTML = function(description) {
   return describeBlockOuter + testOutputOuter + testDescription + '</div>';
 };
 
+
+
+/**
+  Sets a class on the test indicator dependant on if the suite passed or failed.
+**/
 Spec.prototype.run = function() {
   var passedClass = this.success ? 'passed' : 'failed';
   this.testEl.addClass(passedClass);
-}
+};
 
+
+
+/**
+  Tries to a run a test, catches any errors.
+  @param method The test method
+  @return An object containing a boolean for success and an error message.
+**/
 Spec.prototype.runTest = function(method) {
   var errorMessage = '';
   var success = true;
@@ -63,22 +106,38 @@ Spec.prototype.runTest = function(method) {
     passed: success,
     error: errorMessage,
   }
-}
+};
 
+
+
+/**
+  Combines all the test suite's results into an HTML blob.
+  @return The combined test results.
+**/
 Spec.prototype.output = function() {
   return this.results.join('');
-}
+};
 
+
+
+/**
+  Runs a test block within the test suite and adds the results to the HTML.
+**/
 Spec.prototype.describe = function(description, method) {
   this.results.push(this.testDescribeHTML(description));
   method();
   this.results.push('</div>');
 };
 
+
+
+/**
+  Runs a specific test within a test block and adds the results to the HTML.
+**/
 Spec.prototype.it = function(description, method) {
   var testResults = this.runTest(method);
   this.success = this.success && testResults.passed;
   this.results.push(this.testResultsHTML(description, testResults));
-}
+};
 
 var expect = chai.expect;
